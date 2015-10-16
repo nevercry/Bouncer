9b666266e78e8aba5c1b2771e24763341253bf47
nevercry <ysfnevercry@gmail.com>
2015-10-16 23:54:59 +0800

    Learning swift

--- Bouncer/ViewController.swift
+++ Bouncer/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let bouncer = BouncerBehavior()
+    lazy var animator: UIDynamicAnimator = { UIDynamicAnimator(referenceView: self.view) }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        animator.addBehavior(bouncer)
+    }
+    
+    struct Constants {
+        static let BlockSize = CGSize(width: 40, height: 40)
+    }
+    
+    var redBlock: UIView?
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if redBlock == nil {
+            redBlock = addBlock()
+            redBlock?.backgroundColor = UIColor.redColor()
+            bouncer.addBlock(redBlock!)
+        }
+        let motionManager = AppDelegate.Motion.Manager
+        if motionManager.accelerometerAvailable {
+            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue()) {
+                (data, error) -> Void in
+                if data != nil {
+                    self.bouncer.gravity.gravityDirection = CGVectorMake(CGFloat((data!.acceleration.x)), -CGFloat(data!.acceleration.y))
+                }
+                
+            }
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        AppDelegate.Motion.Manager.stopAccelerometerUpdates()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addBlock() -> UIView {
+        let block = UIView(frame: CGRect(origin: CGPoint.zero, size: Constants.BlockSize))
+        block.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        view.addSubview(block)
+        return block
     }
-
-
 }
 
